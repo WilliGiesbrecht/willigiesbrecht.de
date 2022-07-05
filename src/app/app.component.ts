75c5400af679420761e67175b42ded1151b61119
Willi Giesbrecht <97034768+WilliGiesbrecht@users.noreply.github.com>
2022-07-05 20:35:31 +0200

    finish impressum and data-protection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  constructor() { }
+export class AppComponent {
+  currentRoute = '';
 
-  ngOnInit(): void {
-  }
+  constructor(public router: Router) {}
+
 }
